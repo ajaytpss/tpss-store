4bc5ecb870e7ab5737fc1b837896df19e96f435c
officialtpss <officialtpss@gmail.com>
2023-09-04 14:13:06 +0530

    sdf

--- src/db/db.js
+++ src/db/db.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
-const dbUrl = `mongodb+srv://${process.env.DATABASE_USSERNAME}:${process.env.DATABASE_PASSWORD}@cluster0.sqmep40.mongodb.net/${process.env.DATABASE_NAME}?retryWrites=true&w=majority`;
+// const dbUrl = `mongodb+srv://${process.env.DATABASE_USSERNAME}:${process.env.DATABASE_PASSWORD}@cluster0.sqmep40.mongodb.net/${process.env.DATABASE_NAME}?retryWrites=true&w=majority`;
+const dbUrl = `mongodb+srv://ajaykumartpss:BvfZXlKgEF4fZ52P@cluster0.sqmep40.mongodb.net/tpss-store?retryWrites=true&w=majority`;
 
 mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;

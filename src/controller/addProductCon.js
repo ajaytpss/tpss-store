6708b5b41d90de701e2243cc2ab31c56e675d64b
officialtpss <officialtpss@gmail.com>
2023-09-04 13:58:22 +0530

    env

--- src/controller/addProductCon.js
+++ src/controller/addProductCon.js
@@ -1,7 +1,13 @@
 const product = require("../models/product");
 
-const addProduct = (req, res) => {
-  res.send("Add product");
+const addProduct = async (req, res) => {
+  try {
+    const Product = new product(req.body);
+    await Product.save();
+    res.status(200).json({ msg: "Product added successfully!" });
+  } catch (error) {
+    res.status(403).json({ msg: error.errors });
+  }
 };
 
 module.exports = addProduct;
